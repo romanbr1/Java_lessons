10d99e537043f733381941d210f2c7744a1bbcfc
Roman <romanbr@meta.ua>
2018-10-02 08:29:30 +0300

    Add files via upload

---
+++ task23/main.java
@@ -0,0 +1,19 @@
+package ua.lviv.lgs.task23;
+
+import java.util.Random;
+
+public class main {
+
+	public static void main(String[] args) {
+int i =0;
+		do{
+			coin.getRandomBoolean();
+		i++;	
+		}while (i<12);
+		System.out.println("max float = "+Float.MAX_VALUE);
+		System.out.println("min float = "+Float.MIN_VALUE);
+		System.out.println("max double = "+Double.MAX_VALUE);
+		System.out.println("min double = "+Double.MIN_VALUE);
+	}
+	
+}
---
+++ task5/Amphibia.java
@@ -0,0 +1,19 @@
+package ua.lviv.lgs.task5;
+
+public abstract class Amphibia {
+
+	public void eat() {
+		System.out.println("I like eating");
+	}
+	public void sleep() {
+		System.out.println("I like  to sleep");
+	}
+	public void walk() {
+		System.out.println("I like to walk");
+	}
+	public void swim() {
+		System.out.println("I like swimming");
+	}
+	
+	
+}
