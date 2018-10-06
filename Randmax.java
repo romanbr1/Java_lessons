e0abea23e645f294b119b37902635c57ee5e2062
Roman <romanbr@meta.ua>
2018-10-06 09:24:28 +0300

    Add files via upload

---
+++ Randmax.java
@@ -0,0 +1,10 @@
+
+public class Randmax implements Randomablemax{
+
+	@Override
+	public int randmax(int r) {
+			int random_number = (int) (Math.random() * r); 
+			 return random_number;
+	}
+
+}
---
+++ SpecialFeatures.java
@@ -0,0 +1,6 @@
+
+public interface SpecialFeatures {
+ void turbo();
+ void stels();
+ void nuclearStrike();
+}
